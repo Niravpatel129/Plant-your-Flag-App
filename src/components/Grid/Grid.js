e9a29675e88d618ca9b8fa015493214bbfd32998
Niravpatel129 <niravpatelp129@gmail.com>
2019-12-19 16:44:50 -0500

    added frontend

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,17 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Grid.css";
 
-class Grid extends Component {
+class Grid extends React.Component {
   gridDiamter = 10;
-
   items = [];
+  state = {
+    clickedItems: []
+  };
 
-  whenMouseOver = e => {};
+  handleMouseClick = e => {
+    console.log(e.target.id);
+    this.setState({
+      clickedItems: [...this.state.clickedItems, parseInt(e.target.id, 0)]
+    });
+
+    console.log(this.state.clickedItems);
+  };
+
   renderGrid = () => {
-    for (let i = 0; i < window.outerWidth * 5; i++) {
-      this.items.push(
-        <div key={i} className="Grid" onMouseOver={this.whenMouseOver}></div>
-      );
+    this.items = [];
+    for (let i = 0; i < window.outerWidth * 4; i++) {
+      if (this.state.clickedItems.includes(i)) {
+        if (i === this.state.clickedItems[this.state.clickedItems.indexOf(i)]) {
+          this.items.push(
+            <div
+              key={i.toString()}
+              className="Grid"
+              id={i}
+              style={{ backgroundColor: "red" }}
+              onClick={this.handleMouseClick}
+            ></div>
+          );
+        }
+      } else {
+        this.items.push(
+          <div
+            key={i.toString()}
+            className="Grid"
+            id={i}
+            onClick={this.handleMouseClick}
+          ></div>
+        );
+      }
     }
     return this.items;
   };
