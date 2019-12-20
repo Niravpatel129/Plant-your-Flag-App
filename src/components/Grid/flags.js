1c0212e2e2463f74d17ebc611fd3d0a3db5ef1f0
Niravpatel129 <niravpatelp129@gmail.com>
2019-12-19 23:46:42 -0500

    more flags

--- src/components/Grid/flags.js
+++ src/components/Grid/flags.js
@@ -1,6 +1,13 @@
 import ad from "./flags/ad.png";
 import us from "./flags/us.png";
 import ca from "./flags/ca.png";
+import zw from "./flags/zw.png";
+import za from "./flags/za.png";
+import ye from "./flags/ye.png";
+import xk from "./flags/xk.png";
+import ws from "./flags/ws.png";
+import vu from "./flags/vu.png";
+import dm from "./flags/dm.png";
 
 const getFlag = (value, param2) => {
   let newValue;
@@ -16,6 +23,20 @@ const getFlag = (value, param2) => {
       return us;
     case "ca":
       return ca;
+    case "zw":
+      return zw;
+    case "za":
+      return za;
+    case "ye":
+      return ye;
+    case "xk":
+      return xk;
+    case "ws":
+      return ws;
+    case "vu":
+      return vu;
+    case "dm":
+      return dm;
     default:
       return ad;
   }
