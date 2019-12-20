642f674f4e3672f42bbf3d516da21d26e407d20d
Niravpatel129 <niravpatelp129@gmail.com>
2019-12-20 01:05:46 -0500

    added all flags

--- src/components/Grid/flags.js
+++ src/components/Grid/flags.js
@@ -1,16 +1,203 @@
 import ad from "./flags/ad.png";
-import us from "./flags/us.png";
-import ca from "./flags/ca.png";
-import zw from "./flags/zw.png";
-import za from "./flags/za.png";
-import ye from "./flags/ye.png";
-import xk from "./flags/xk.png";
-import ws from "./flags/ws.png";
-import vu from "./flags/vu.png";
-import dm from "./flags/dm.png";
+import ae from "./flags/ae.png";
+import af from "./flags/af.png";
+import ag from "./flags/ag.png";
+import al from "./flags/al.png";
+import am from "./flags/am.png";
+import ao from "./flags/ao.png";
+import ar from "./flags/ar.png";
+import at from "./flags/at.png";
 import au from "./flags/au.png";
+import az from "./flags/az.png";
 import ba from "./flags/ba.png";
+import bb from "./flags/bb.png";
+import bd from "./flags/bd.png";
+import be from "./flags/be.png";
+import bf from "./flags/bf.png";
+import bg from "./flags/bg.png";
+import bh from "./flags/bh.png";
+import bi from "./flags/bi.png";
+import bj from "./flags/bj.png";
+import bn from "./flags/bn.png";
+import bo from "./flags/bo.png";
+import br from "./flags/br.png";
+import bs from "./flags/bs.png";
+import bt from "./flags/bt.png";
+import bw from "./flags/bw.png";
+import by from "./flags/by.png";
+import bz from "./flags/bz.png";
+import ca from "./flags/ca.png";
+import cd from "./flags/cd.png";
+import cf from "./flags/cf.png";
+import cg from "./flags/cg.png";
+import ch from "./flags/ch.png";
+import ci from "./flags/ci.png";
+import ck from "./flags/ck.png";
+import cl from "./flags/cl.png";
+import cm from "./flags/cm.png";
+import cn from "./flags/cn.png";
+import co from "./flags/co.png";
+import cr from "./flags/cr.png";
+import cu from "./flags/cu.png";
+import cv from "./flags/cv.png";
+import cy from "./flags/cy.png";
+import cz from "./flags/cz.png";
 import de from "./flags/de.png";
+import dj from "./flags/dj.png";
+import dk from "./flags/dk.png";
+import dm from "./flags/dm.png";
+import dz from "./flags/dz.png";
+import ec from "./flags/ec.png";
+import ee from "./flags/ee.png";
+import eg from "./flags/eg.png";
+import eh from "./flags/eh.png";
+import er from "./flags/er.png";
+import es from "./flags/es.png";
+import et from "./flags/et.png";
+import fi from "./flags/fi.png";
+import fj from "./flags/fj.png";
+import fm from "./flags/fm.png";
+import fr from "./flags/fr.png";
+import ga from "./flags/ga.png";
+import gb from "./flags/gb.png";
+import gd from "./flags/gd.png";
+import ge from "./flags/ge.png";
+import gh from "./flags/gh.png";
+import gm from "./flags/gm.png";
+import gn from "./flags/gn.png";
+import gq from "./flags/gq.png";
+import gr from "./flags/gr.png";
+import gt from "./flags/gt.png";
+import gw from "./flags/gw.png";
+import gy from "./flags/gy.png";
+import hn from "./flags/hn.png";
+import hr from "./flags/hr.png";
+import ht from "./flags/ht.png";
+import hu from "./flags/hu.png";
+import id from "./flags/id.png";
+import ie from "./flags/ie.png";
+import il from "./flags/il.png";
+import doo from "./flags/doo.png";
+import inn from "./flags/inn.png";
+import iq from "./flags/iq.png";
+import ir from "./flags/ir.png";
+import is from "./flags/is.png";
+import it from "./flags/it.png";
+import jm from "./flags/jm.png";
+import jo from "./flags/jo.png";
+import jp from "./flags/jp.png";
+import ke from "./flags/ke.png";
+import kg from "./flags/kg.png";
+import kh from "./flags/kh.png";
+import ki from "./flags/ki.png";
+import km from "./flags/km.png";
+import kn from "./flags/kn.png";
+import kp from "./flags/kp.png";
+import kr from "./flags/kr.png";
+import kw from "./flags/kw.png";
+import kz from "./flags/kz.png";
+import la from "./flags/la.png";
+import lb from "./flags/lb.png";
+import lc from "./flags/lc.png";
+import li from "./flags/li.png";
+import lk from "./flags/lk.png";
+import lr from "./flags/lr.png";
+import ls from "./flags/ls.png";
+import lt from "./flags/lt.png";
+import lu from "./flags/lu.png";
+import lv from "./flags/lv.png";
+import ly from "./flags/ly.png";
+import ma from "./flags/ma.png";
+import mc from "./flags/mc.png";
+import md from "./flags/md.png";
+import me from "./flags/me.png";
+import mg from "./flags/mg.png";
+import mh from "./flags/mh.png";
+import mk from "./flags/mk.png";
+import ml from "./flags/ml.png";
+import mm from "./flags/mm.png";
+import mn from "./flags/mn.png";
+import mr from "./flags/mr.png";
+import mt from "./flags/mt.png";
+import mu from "./flags/mu.png";
+import mv from "./flags/mv.png";
+import mw from "./flags/mw.png";
+import mx from "./flags/mx.png";
+import my from "./flags/my.png";
+import mz from "./flags/mz.png";
+import na from "./flags/na.png";
+import ne from "./flags/ne.png";
+import ng from "./flags/ng.png";
+import ni from "./flags/ni.png";
+import nl from "./flags/nl.png";
+import no from "./flags/no.png";
+import np from "./flags/np.png";
+import nr from "./flags/nr.png";
+import nu from "./flags/nu.png";
+import nz from "./flags/nz.png";
+import om from "./flags/om.png";
+import pa from "./flags/pa.png";
+import pe from "./flags/pe.png";
+import pg from "./flags/pg.png";
+import ph from "./flags/ph.png";
+import pk from "./flags/pk.png";
+import pl from "./flags/pl.png";
+import ps from "./flags/ps.png";
+import pt from "./flags/pt.png";
+import pw from "./flags/pw.png";
+import py from "./flags/py.png";
+import qa from "./flags/qa.png";
+import ro from "./flags/ro.png";
+import rs from "./flags/rs.png";
+import ru from "./flags/ru.png";
+import rw from "./flags/rw.png";
+import sa from "./flags/sa.png";
+import sb from "./flags/sb.png";
+import sc from "./flags/sc.png";
+import sd from "./flags/sd.png";
+import se from "./flags/se.png";
+import sg from "./flags/sg.png";
+import si from "./flags/si.png";
+import sk from "./flags/sk.png";
+import sl from "./flags/sl.png";
+import sm from "./flags/sm.png";
+import sn from "./flags/sn.png";
+import so from "./flags/so.png";
+import sr from "./flags/sr.png";
+import ss from "./flags/ss.png";
+import st from "./flags/st.png";
+import sv from "./flags/sv.png";
+import sy from "./flags/sy.png";
+import sz from "./flags/sz.png";
+import td from "./flags/td.png";
+import tg from "./flags/tg.png";
+import th from "./flags/th.png";
+import tj from "./flags/tj.png";
+import tl from "./flags/tl.png";
+import tm from "./flags/tm.png";
+import tn from "./flags/tn.png";
+import to from "./flags/to.png";
+import tr from "./flags/tr.png";
+import tt from "./flags/tt.png";
+import tv from "./flags/tv.png";
+import tw from "./flags/tw.png";
+import tz from "./flags/tz.png";
+import ua from "./flags/ua.png";
+import ug from "./flags/ug.png";
+import us from "./flags/us.png";
+import uy from "./flags/uy.png";
+import uz from "./flags/uz.png";
+import va from "./flags/va.png";
+import vc from "./flags/vc.png";
+import ve from "./flags/ve.png";
+import vn from "./flags/vn.png";
+import vu from "./flags/vu.png";
+import ws from "./flags/ws.png";
+import xk from "./flags/xk.png";
+import ye from "./flags/ye.png";
+import za from "./flags/za.png";
+import zm from "./flags/zm.png";
+import zw from "./flags/zw.png";
 
 const getFlag = (value, param2) => {
   let newValue;
@@ -22,30 +209,404 @@ const getFlag = (value, param2) => {
   switch (newValue) {
     case "ad":
       return ad;
-    case "de":
-      return de;
-    case "us":
-      return us;
-    case "ca":
-      return ca;
-    case "ba":
-      return ba;
-    case "zw":
-      return zw;
-    case "za":
-      return za;
-    case "ye":
-      return ye;
-    case "xk":
-      return xk;
-    case "ws":
-      return ws;
-    case "vu":
-      return vu;
-    case "dm":
-      return dm;
+    case "ae":
+      return ae;
+    case "af":
+      return af;
+    case "ag":
+      return ag;
+    case "al":
+      return al;
+    case "am":
+      return am;
+    case "ao":
+      return ao;
+    case "ar":
+      return ar;
+    case "at":
+      return at;
     case "au":
       return au;
+    case "az":
+      return az;
+    case "ba":
+      return ba;
+    case "bb":
+      return bb;
+    case "bd":
+      return bd;
+    case "be":
+      return be;
+    case "bf":
+      return bf;
+    case "bg":
+      return bg;
+    case "bh":
+      return bh;
+    case "bi":
+      return bi;
+    case "bj":
+      return bj;
+    case "bn":
+      return bn;
+    case "bo":
+      return bo;
+    case "br":
+      return br;
+    case "bs":
+      return bs;
+    case "bt":
+      return bt;
+    case "bw":
+      return bw;
+    case "by":
+      return by;
+    case "bz":
+      return bz;
+    case "ca":
+      return ca;
+    case "cd":
+      return cd;
+    case "cf":
+      return cf;
+    case "cg":
+      return cg;
+    case "ch":
+      return ch;
+    case "ci":
+      return ci;
+    case "ck":
+      return ck;
+    case "cl":
+      return cl;
+    case "cm":
+      return cm;
+    case "cn":
+      return cn;
+    case "co":
+      return co;
+    case "cr":
+      return cr;
+    case "cu":
+      return cu;
+    case "cv":
+      return cv;
+    case "cy":
+      return cy;
+    case "cz":
+      return cz;
+    case "de":
+      return de;
+    case "dj":
+      return dj;
+    case "dk":
+      return dk;
+    case "dm":
+      return dm;
+    case "do":
+      return doo;
+    case "dz":
+      return dz;
+    case "ec":
+      return ec;
+    case "ee":
+      return ee;
+    case "eg":
+      return eg;
+    case "eh":
+      return eh;
+    case "er":
+      return er;
+    case "es":
+      return es;
+    case "et":
+      return et;
+    case "fi":
+      return fi;
+    case "fj":
+      return fj;
+    case "fm":
+      return fm;
+    case "fr":
+      return fr;
+    case "ga":
+      return ga;
+    case "gb":
+      return gb;
+    case "gd":
+      return gd;
+    case "ge":
+      return ge;
+    case "gh":
+      return gh;
+    case "gm":
+      return gm;
+    case "gn":
+      return gn;
+    case "gq":
+      return gq;
+    case "gr":
+      return gr;
+    case "gt":
+      return gt;
+    case "gw":
+      return gw;
+    case "gy":
+      return gy;
+    case "hn":
+      return hn;
+    case "hr":
+      return hr;
+    case "ht":
+      return ht;
+    case "hu":
+      return hu;
+    case "id":
+      return id;
+    case "ie":
+      return ie;
+    case "il":
+      return il;
+    case "in":
+      return inn;
+    case "iq":
+      return iq;
+    case "ir":
+      return ir;
+    case "is":
+      return is;
+    case "it":
+      return it;
+    case "jm":
+      return jm;
+    case "jo":
+      return jo;
+    case "jp":
+      return jp;
+    case "ke":
+      return ke;
+    case "kg":
+      return kg;
+    case "kh":
+      return kh;
+    case "ki":
+      return ki;
+    case "km":
+      return km;
+    case "kn":
+      return kn;
+    case "kp":
+      return kp;
+    case "kr":
+      return kr;
+    case "kw":
+      return kw;
+    case "kz":
+      return kz;
+    case "la":
+      return la;
+    case "lb":
+      return lb;
+    case "lc":
+      return lc;
+    case "li":
+      return li;
+    case "lk":
+      return lk;
+    case "lr":
+      return lr;
+    case "ls":
+      return ls;
+    case "lt":
+      return lt;
+    case "lu":
+      return lu;
+    case "lv":
+      return lv;
+    case "ly":
+      return ly;
+    case "ma":
+      return ma;
+    case "mc":
+      return mc;
+    case "md":
+      return md;
+    case "me":
+      return me;
+    case "mg":
+      return mg;
+    case "mh":
+      return mh;
+    case "mk":
+      return mk;
+    case "ml":
+      return ml;
+    case "mm":
+      return mm;
+    case "mn":
+      return mn;
+    case "mr":
+      return mr;
+    case "mt":
+      return mt;
+    case "mu":
+      return mu;
+    case "mv":
+      return mv;
+    case "mw":
+      return mw;
+    case "mx":
+      return mx;
+    case "my":
+      return my;
+    case "mz":
+      return mz;
+    case "na":
+      return na;
+    case "ne":
+      return ne;
+    case "ng":
+      return ng;
+    case "ni":
+      return ni;
+    case "nl":
+      return nl;
+    case "no":
+      return no;
+    case "np":
+      return np;
+    case "nr":
+      return nr;
+    case "nu":
+      return nu;
+    case "nz":
+      return nz;
+    case "om":
+      return om;
+    case "pa":
+      return pa;
+    case "pe":
+      return pe;
+    case "pg":
+      return pg;
+    case "ph":
+      return ph;
+    case "pk":
+      return pk;
+    case "pl":
+      return pl;
+    case "ps":
+      return ps;
+    case "pt":
+      return pt;
+    case "pw":
+      return pw;
+    case "py":
+      return py;
+    case "qa":
+      return qa;
+    case "ro":
+      return ro;
+    case "rs":
+      return rs;
+    case "ru":
+      return ru;
+    case "rw":
+      return rw;
+    case "sa":
+      return sa;
+    case "sb":
+      return sb;
+    case "sc":
+      return sc;
+    case "sd":
+      return sd;
+    case "se":
+      return se;
+    case "sg":
+      return sg;
+    case "si":
+      return si;
+    case "sk":
+      return sk;
+    case "sl":
+      return sl;
+    case "sm":
+      return sm;
+    case "sn":
+      return sn;
+    case "so":
+      return so;
+    case "sr":
+      return sr;
+    case "ss":
+      return ss;
+    case "st":
+      return st;
+    case "sv":
+      return sv;
+    case "sy":
+      return sy;
+    case "sz":
+      return sz;
+    case "td":
+      return td;
+    case "tg":
+      return tg;
+    case "th":
+      return th;
+    case "tj":
+      return tj;
+    case "tl":
+      return tl;
+    case "tm":
+      return tm;
+    case "tn":
+      return tn;
+    case "to":
+      return to;
+    case "tr":
+      return tr;
+    case "tt":
+      return tt;
+    case "tv":
+      return tv;
+    case "tw":
+      return tw;
+    case "tz":
+      return tz;
+    case "ua":
+      return ua;
+    case "ug":
+      return ug;
+    case "us":
+      return us;
+    case "uy":
+      return uy;
+    case "uz":
+      return uz;
+    case "va":
+      return va;
+    case "vc":
+      return vc;
+    case "ve":
+      return ve;
+    case "vn":
+      return vn;
+    case "vu":
+      return vu;
+    case "ws":
+      return ws;
+    case "xk":
+      return xk;
+    case "ye":
+      return ye;
+    case "za":
+      return za;
+    case "zm":
+      return zm;
+    case "zw":
+      return zw;
     default:
       return ca;
   }
